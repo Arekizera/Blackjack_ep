85bcdbfb4b3f303f16562b6a1abca8862a45c32c
Arekizera <49684260+Arekizera@users.noreply.github.com>
2019-10-16 21:26:24 -0300

    Update BLACKJACK1.py

--- BLACKJACK1.py
+++ BLACKJACK1.py
@@ -8,3 +8,14 @@ J = 10 #todas com figura valem 10 pontos
 Q = 10
 K = 10
 A = 11 #vale 1 se a mao for estourar
+baralhoinicial = [A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K]*4 #cada baralho tem 4 desses
+naipes = ["paus", "copas", "espadas", "ouros"]
+baralho = (baralhoinicial)
+maojog = [] #mão do jogador
+maocro = [] #mão do croupier
+
+
+lista_continuar = ["continuar", "Continuar", "CONTINUAR"] # lista de possibilidade de "continuar"
+lista_carta=["carta", "cartas", "Carta", "Cartas", "CARTA", 'CARTAS', 'c', "C"]  #lista de possibilidades de "carta"
+
+
