f42f7da7f3d0038c54cd7544013db3e7c722516a
Bruno Tosi <55807654+brunotosii@users.noreply.github.com>
2019-10-16 21:25:34 -0300

    Update BLACKJACK1.py

--- BLACKJACK1.py
+++ BLACKJACK1.py
@@ -1,1 +1,10 @@
+import random
 
+#Alunos:
+#- Alex Steijntjes, alexs4@al.insper.edu.br
+#- Bruno Tosi, brunoct1@al.insper.edu.br
+
+J = 10 #todas com figura valem 10 pontos
+Q = 10
+K = 10
+A = 11 #vale 1 se a mao for estourar
