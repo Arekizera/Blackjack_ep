0357769fa5c87ad0373d661f9f5e1c31cef78057
Arekizera <49684260+Arekizera@users.noreply.github.com>
2019-10-16 21:27:35 -0300

    Update BLACKJACK1.py

--- BLACKJACK1.py
+++ BLACKJACK1.py
@@ -28,3 +28,13 @@ while pergunta != "s" or pergunta != "n":
     if pergunta == "n":
         print('\n'
               'Blackjack Insper!\n'
+              \n' 
+              'BlackJack é um jogo cujo o objetivo é somar 21 pontos com a soma dos valores das cartas.\n'
+              '\n'
+              'Como jogar:\n'
+              '• Inicialmente, é feito uma aposta por parte do jogador, e recebendo duas cartas do baralho.\n'
+              '• O Croupier também recebe duas cartas, porém você não sabe quais são.\n'
+              '• Caso o valor das cartas passe de 21 pontos, você ou o Croupier perdem.\n'
+              '\n'
+              'Regras do Jogo:\n'
+              '• O jogo é composto de no maximo 10 baralhos de 52 cartas cada.\n'
